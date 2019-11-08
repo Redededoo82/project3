3c421b92808feee68fd8a406207dd356de3aeedc
AARON M <asmarcks@gmail.com>
2019-11-08 14:19:50 -0600

    modified UI a little

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import './Forms.css';
-import LoginForm from  './LoginForm';
-import SignUpForm from './SignUpForm';
+import LoginForm from  './pages/LoginForm';
+import SignUpForm from './pages/SignUpForm';
+import AboutPage from './pages/AboutPage';
 
 
 function App() {
   return (
     <div className="App">
-
+          <AboutPage></AboutPage>
           <SignUpForm></SignUpForm>
           <br></br>
           <LoginForm></LoginForm>
