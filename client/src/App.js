c04c172c4ac546aeaf34feb55aad2bffe3015ef3
AARON M <asmarcks@gmail.com>
2019-11-07 18:27:19 -0600

    sign up and login rendered

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './App.css';
 import LoginForm from  './LoginForm';
+import SignUpForm from './SignUpForm';
 
 function App() {
   return (
     <div className="App">
+          <SignUpForm></SignUpForm>
           <LoginForm></LoginForm>
     </div>
   );
