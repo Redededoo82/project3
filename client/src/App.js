0dc306c4ec6434126aac582cc5a00cbc6aece47b
AARON M <asmarcks@gmail.com>
2019-11-10 13:33:17 -0600

    events page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './Forms.css';
+// import './Forms.css';
 import LoginForm from './LoginForm';
 import SignUpForm from './SignUpForm';
 import AboutPage from './AboutPage';
 import Home from './Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './Nav';
+import Events from './Events';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <div className="App">
         <Nav />
         <Switch>
+        <Route path="/events" component={Events}/>
         <Route path="/about" component={AboutPage}/>
         <Route path="/signup" component={SignUpForm}/>
         <Route path="/login" component={LoginForm}/>
