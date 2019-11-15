99d38fcc5ed4e6ab1b2a52515e716d77bbd01fd8
William Lukas <iggyboy456@gmail.com>
2019-11-15 10:57:17 -0600

    fixed this stupid ggarbage

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://booty420:donkeyboner420@ds033559.mlab.com:33559/heroku_njg72qjp");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/leavenotrace");
 
 app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on http://localhost: ${PORT}!`);
